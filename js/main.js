bc7c7a77d396d19fb7929c9ad344669ea1de3f82
jimm_mm <98532788+jimm-8@users.noreply.github.com>
2022-02-23 17:06:46 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// change navbar type on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// navbar for tablets (show/hide menu)
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu for tablets
+function closeNav() {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
